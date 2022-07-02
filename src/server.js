6ebc605707f9134781538cafed47fe708994d77c
Ted Fitzpatrick <tedfitzpatrick@me.com>
2022-07-02 11:31:41 -0700

    added try catch to user endpoint

--- src/server.js
+++ src/server.js
@@ -8,14 +8,18 @@ app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/user', (req, res) => {
+app.get('/api/user', async (req, res) => {
   
+  try {
   const mongoclient = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
   const db = mongoclient.db('resumewebsitedb');
   const user = await db.collection('user').findOne({});
   res.status(200).json(user);
   mongoclient.close();
-  
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
 });
 
 app.listen(8000, () => {
